5e918318ca5ce1d67d4642c5ef88c1b622d30577
akm <akm2000@gmail.com>
2025-03-30 10:56:17 +0900

    🔧 Copy elements except components from Responsive Sidebar Layout
    https://flowbite-svelte.com/docs/examples/sidebar-layout
    
    https://github.com/shinokada/flowbite-sveltekit-responsive-sidebar-layout/blob/main/src/routes/%2Blayout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,12 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<header class="flex-none w-full mx-auto bg-white dark:bg-slate-950">
+	
+</header>
+
+<div class="flex px-4 mx-auto w-full">
+	<main class="lg:ml-72 w-full mx-auto">
+		{@render children()}
+	</main>
+</div>
