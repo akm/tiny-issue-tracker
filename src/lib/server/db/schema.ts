2e39d68548bed1e7b0ea9f98694990e1c0445923
akm <akm2000@gmail.com>
2025-03-30 20:07:59 +0900

    🩹 Remove duplicated definitions from schema.ts

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,11 +1,6 @@
-import mysql from 'mysql2/promise';
 import { mysqlTable, int, varchar, datetime } from 'drizzle-orm/mysql-core';
-import { drizzle } from 'drizzle-orm/mysql2';
 
 import type { InferSelectModel } from 'drizzle-orm';
-
-const connection = await mysql.createConnection();
-const db = drizzle(connection);
 
 export const userTable = mysqlTable('user', {
 	id: int('id').primaryKey().autoincrement()
