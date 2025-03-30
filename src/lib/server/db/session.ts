b36ad551e1ff3fac0e7fb3e1b0756a2d84bf2517
akm <akm2000@gmail.com>
2025-03-30 21:20:19 +0900

    ✨ Implement validateSessionToken

--- src/lib/server/db/session.ts
+++ src/lib/server/db/session.ts
@@ -1,6 +1,9 @@
+import { eq } from 'drizzle-orm';
+
 import type { User, Session } from './schema';
-import { sessionTable } from './schema';
+import { userTable, sessionTable } from './schema';
 import { db } from './index';
+
 import { encodeBase32LowerCaseNoPadding, encodeHexLowerCase } from '@oslojs/encoding';
 import { sha256 } from '@oslojs/crypto/sha2';
 
@@ -23,7 +26,30 @@ export async function createSession(token: string, userId: number): Promise<Sess
 }
 
 export async function validateSessionToken(token: string): Promise<SessionValidationResult> {
-	// TODO
+	const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+	const result = await db
+		.select({ user: userTable, session: sessionTable })
+		.from(sessionTable)
+		.innerJoin(userTable, eq(sessionTable.userId, userTable.id))
+		.where(eq(sessionTable.id, sessionId));
+	if (result.length < 1) {
+		return { session: null, user: null };
+	}
+	const { user, session } = result[0];
+	if (Date.now() >= session.expiresAt.getTime()) {
+		await db.delete(sessionTable).where(eq(sessionTable.id, session.id));
+		return { session: null, user: null };
+	}
+	if (Date.now() >= session.expiresAt.getTime() - 1000 * 60 * 60 * 24 * 15) {
+		session.expiresAt = new Date(Date.now() + 1000 * 60 * 60 * 24 * 30);
+		await db
+			.update(sessionTable)
+			.set({
+				expiresAt: session.expiresAt
+			})
+			.where(eq(sessionTable.id, session.id));
+	}
+	return { session, user };
 }
 
 export async function invalidateSession(sessionId: string): Promise<void> {
