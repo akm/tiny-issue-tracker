6f21f271d95188af599c7e1bd322e597665c2457
akm <akm2000@gmail.com>
2025-03-30 20:05:33 +0900

    ✨ Copy schema declaration from https://lucia-auth.com/sessions/basic-api/drizzle-orm

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,25 @@
-import { mysqlTable, serial, text, int } from 'drizzle-orm/mysql-core';
+import mysql from 'mysql2/promise';
+import { mysqlTable, int, varchar, datetime } from 'drizzle-orm/mysql-core';
+import { drizzle } from 'drizzle-orm/mysql2';
 
-export const user = mysqlTable('user', {
-	id: serial('id').primaryKey(),
-	age: int('age')
+import type { InferSelectModel } from 'drizzle-orm';
+
+const connection = await mysql.createConnection();
+const db = drizzle(connection);
+
+export const userTable = mysqlTable('user', {
+	id: int('id').primaryKey().autoincrement()
 });
+
+export const sessionTable = mysqlTable('session', {
+	id: varchar('id', {
+		length: 255
+	}).primaryKey(),
+	userId: int('user_id')
+		.notNull()
+		.references(() => userTable.id),
+	expiresAt: datetime('expires_at').notNull()
+});
+
+export type User = InferSelectModel<typeof userTable>;
+export type Session = InferSelectModel<typeof sessionTable>;
