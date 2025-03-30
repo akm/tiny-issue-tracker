f17f42628e2699e5f55845934252a538ab42d1fb
akm <akm2000@gmail.com>
2025-03-30 22:09:27 +0900

    ✨ Add hooks.server.ts  with handle function

---
+++ src/hooks.server.ts
@@ -0,0 +1,24 @@
+import { setSessionTokenCookie, deleteSessionTokenCookie } from './lib/server/session';
+import { validateSessionToken } from './lib/server/db/session';
+
+import type { Handle } from '@sveltejs/kit';
+
+export const handle: Handle = async ({ event, resolve }) => {
+	const token = event.cookies.get('session') ?? null;
+	if (token === null) {
+		event.locals.user = null;
+		event.locals.session = null;
+		return resolve(event);
+	}
+
+	const { session, user } = await validateSessionToken(token);
+	if (session !== null) {
+		setSessionTokenCookie(event, token, session.expiresAt);
+	} else {
+		deleteSessionTokenCookie(event);
+	}
+
+	event.locals.session = session;
+	event.locals.user = user;
+	return resolve(event);
+};
