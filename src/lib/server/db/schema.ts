4c5adb52577253d9ce9c60c201c3c599ac5fc17b
akm <akm2000@gmail.com>
2025-03-30 22:41:17 +0900

    ✨ Add google_id and name columns to user table
    https://lucia-auth.com/tutorials/google-oauth/sveltekit

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -3,7 +3,9 @@ import { mysqlTable, int, varchar, datetime } from 'drizzle-orm/mysql-core';
 import type { InferSelectModel } from 'drizzle-orm';
 
 export const userTable = mysqlTable('user', {
-	id: int('id').primaryKey().autoincrement()
+	id: int('id').primaryKey().autoincrement(),
+	googleId: varchar('google_id', { length: 255 }).notNull().unique(),
+	name: varchar('name', { length: 255 }).notNull()
 });
 
 export const sessionTable = mysqlTable('session', {
