11edd75cfa947773b0c19c14937295cda58a8b11
akm <akm2000@gmail.com>
2025-03-31 19:44:17 +0900

    ✨ Add import User and Session in app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,8 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+
+import type { User, Session } from '$lib/server/db/schema';
+
 declare global {
 	namespace App {
 		// interface Error {}
