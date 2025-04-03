37cd995dc402779ba9015e6ab38a73bd8bcc303c
akm <akm2000@gmail.com>
2025-04-03 23:05:40 +0900

    ✨ Chnage column names for users

--- src/routes/(authed)/users/+page.svelte
+++ src/routes/(authed)/users/+page.svelte
@@ -1,19 +1,18 @@
- <script lang="ts">
+ <script >
     import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell } from 'flowbite-svelte';
     let items = [
-        { id: 1, maker: 'Toyota', type: 'ABC', make: 2017 },
-        { id: 2, maker: 'Ford', type: 'CDE', make: 2018 },
-        { id: 3, maker: 'Volvo', type: 'FGH', make: 2019 },
-        { id: 4, maker: 'Saab', type: 'IJK', make: 2020 }
+        { id: 1, name: 'Toyota', email: 'ABC'},
+        { id: 2, name: 'Ford', email: 'CDE' },
+        { id: 3, name: 'Volvo', email: 'FGH' },
+        { id: 4, name: 'Saab', email: 'IJK' }
     ];
 </script>
 
 <Table hoverable={true} {items}>
     <TableHead>
         <TableHeadCell sort={(a, b) => a.id - b.id}>ID</TableHeadCell>
-        <TableHeadCell sort={(a, b) => a.maker.localeCompare(b.maker)} defaultSort>Maker</TableHeadCell>
-        <TableHeadCell sort={(a, b) => a.type.localeCompare(b.type)}>Type</TableHeadCell>
-        <TableHeadCell sort={(a, b) => a.make - b.make} defaultDirection="desc">Make</TableHeadCell>
+        <TableHeadCell sort={(a, b) => a.name.localeCompare(b.maker)} defaultSort>Name</TableHeadCell>
+        <TableHeadCell sort={(a, b) => a.email.localeCompare(b.type)}>Email</TableHeadCell>
         <TableHeadCell>
         <span class="sr-only">Buy</span>
         </TableHeadCell>
@@ -21,9 +20,8 @@
     <TableBody tableBodyClass="divide-y">
         <TableBodyRow slot="row" let:item>
         <TableBodyCell>{item.id}</TableBodyCell>
-        <TableBodyCell>{item.maker}</TableBodyCell>
-        <TableBodyCell>{item.type}</TableBodyCell>
-        <TableBodyCell>{item.make}</TableBodyCell>
+        <TableBodyCell>{item.name}</TableBodyCell>
+        <TableBodyCell>{item.email}</TableBodyCell>
         <TableBodyCell>
             <a href="/tables" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Buy</a>
         </TableBodyCell>
