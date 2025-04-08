7ed2f199a24b9942e4c28b8588a7d3d777b06b83
akm <akm2000@gmail.com>
2025-04-08 22:51:28 +0900

    ✨ Retrieve checked IDs on show delete modal

--- src/routes/(authed)/organizations/+page.svelte
+++ src/routes/(authed)/organizations/+page.svelte
@@ -25,6 +25,8 @@
         };
     };
 
+    let checkedIDs: number[] = $state([]);
+
     let modalState: 'new' | 'edit' = $state('new');
     $inspect({modalState});
 
@@ -89,10 +91,11 @@
     };
 
     const showDeleteModal = async () => {
-        if (!organizationModal) {
+        if (!deleteModal) {
             console.error("showDeleteModal: deleteModal is not defined");
             return;
         }
+        console.log("showDeleteModal", {checkedIDs});
         deleteModal.show();
     }
 
@@ -147,7 +150,10 @@
                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
                     <td class="w-4 p-4">
                         <div class="flex items-center">
-                            <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                            <input id="checkbox-table-search-1" type="checkbox" 
+                                value={item.id}
+                                bind:group={checkedIDs}
+                                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                             <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
                         </div>
                     </td>
