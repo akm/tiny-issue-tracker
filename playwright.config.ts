e7d136d1c6fe6707fd6c22d6c6243ed609b546b6
akm <akm2000@gmail.com>
2025-05-07 20:11:39 +0900

    🔧 Add options to playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,5 +5,15 @@ export default defineConfig({
 		command: 'npm run build && npm run preview',
 		port: 4173
 	},
-	testDir: 'e2e'
+	testDir: 'e2e',
+	use: {
+		headless: process.env.HEADED != 'true',
+		launchOptions: {
+			slowMo: Number(process.env.SLOMO || 0) // テスト実行時のスローモーション。デモなら 2000 くらいがよい
+		},
+		trace: 'retain-on-failure'
+	},
+
+	// https://playwright.dev/docs/test-reporters#github-actions-annotations
+	reporter: process.env.CI ? 'github' : 'list'
 });
